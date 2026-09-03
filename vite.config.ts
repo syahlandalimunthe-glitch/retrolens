52e79b68797818b1863a97ae2feb389ec1b0e233
syahlandalimunthe-glitch <syahlandalimunthe@gmail.com>
2026-09-04 06:23:33 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,10 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+    sourcemap: false,
+    chunkSizeWarningLimit: 1500
+  },
   server: { host: true }
 });
